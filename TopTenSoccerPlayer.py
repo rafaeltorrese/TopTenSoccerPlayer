353b533e7bef716e5470ae63a594f9fa5e5b24a1
rafaeltorrese <torresc.rafael@gmail.com>
2019-10-01 00:20:53 -0500

    Dig into HTML

--- TopTenSoccerPlayer.py
+++ TopTenSoccerPlayer.py
@@ -17,9 +17,11 @@ base_url = "https://en.wikipedia.org/wiki/World_Soccer_(magazine)"
 page = requests.get(base_url)
 
 if page.status_code == requests.codes.ok:
-
     bs = BeautifulSoup(page.text, 'lxml')
 
+# Fin something you specify in th hmtl
+list_of_all_players = bs.find('table' ,class_='multicol').find('ul').find_all('li')
+last_ten_players = list_of_all_players[-10:]
 
 
 
