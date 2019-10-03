3a6113bd067cacbcc6cc36affa364fc4d33c70d6
Rafael Torres Escobar <torresc.rafael@gmail.com>
2019-10-03 13:37:07 -0500

    Scrap Craigslist

---
+++ Craigslist.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  3 13:34:44 2019
+
+@author: rafael
+"""
+
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
